f92d06658e5c3426c538ade6b3b31da287d3a902
Zakky <150591719+zakky20@users.noreply.github.com>
2025-01-03 22:19:22 +0000

    Add files via upload

---
+++ dwm/themes/catppuccin.h
@@ -0,0 +1,6 @@
+static char normbgcolor[]           = "#1e1e2e";
+static char normbordercolor[]       = "#1e1e2e";
+static char normfgcolor[]           = "#cdd6f4";
+static char selfgcolor[]            = "#cdd6f4";
+static char selbordercolor[]        = "#1e1e2e";
+static char selbgcolor[]            = "#1e1e2e"; 
---
+++ dwm/themes/gruvbox.h
@@ -0,0 +1,6 @@
+static char normbgcolor[]           = "#282828";
+static char normbordercolor[]       = "#3c3836";
+static char normfgcolor[]           = "#ebdbb2";
+static char selfgcolor[]            = "#fbf1c7";
+static char selbordercolor[]        = "#d65d03";
+static char selbgcolor[]            = "#d65d0e";
